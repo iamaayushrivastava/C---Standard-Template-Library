fb10ab1dc0c65c8dbdb98040d2532f21eb3033de
Aayush Shrivastava <iamaayushrivastava@gmail.com>
2024-07-03 18:54:50 +0530

    Introduction to STL

---
+++ Introduction.cpp
@@ -0,0 +1,23 @@
+// Introduction to STL
+
+// NOTES
+
+// 1. STL: Standard Template Library
+// 2. It consist of four components
+//    a. Containers
+//    b. Iterators 
+//    c. Algorithm
+//    d. Functors
+
+// a. Containers - Containers or container classes store objects and data.
+
+//    i. Sequence Containers: It implements data structures that can be accessed in a sequential manner
+//    ii. Container Adaptors: It provides a different interface for sequential containers
+//    iii. Associative Containers: It implements sorted data structures that can be quickly searched in O(log n) time complexity
+//    iv. Unordered Associative Containers: implements unordered data structures that can be quickly searched
+
+// b. Iterators - Iterators are used for working on a sequence of values.
+
+// c. Algorithm - It defines a collection of functions specially designed to be used on a range of elements.
+
+// d. Functors - A functor (or function object) is a C++ class that acts like a function.
